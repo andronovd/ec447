d1d54531b2c93606d88c7cbbd60a8d45dc36df39
andronovd <andronov@bu.edu>
2016-10-04 09:17:44 -0400

    work on lab3

--- Lab3/Lab3/Form1.Designer.cs
+++ Lab3/Lab3/Form1.Designer.cs
@@ -28,22 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Clear = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // Clear
+            // 
+            this.Clear.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.Clear.Location = new System.Drawing.Point(94, 201);
+            this.Clear.Name = "Clear";
+            this.Clear.Size = new System.Drawing.Size(75, 23);
+            this.Clear.TabIndex = 0;
+            this.Clear.Text = "Clear";
+            this.Clear.UseVisualStyleBackColor = true;
+            this.Clear.Click += new System.EventHandler(this.Clear_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(284, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(46, 20);
+            this.clearToolStripMenuItem.Text = "&Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.Clear_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Clear);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
             this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button Clear;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
     }
 }
 
--- Lab3/Lab3/Form1.cs
+++ Lab3/Lab3/Form1.cs
@@ -14,7 +14,11 @@ namespace Lab3
     public partial class Form1 : Form
     {
         private ArrayList circles = new ArrayList();
-        public int radius = 20;
+        private ArrayList remove_rbc = new ArrayList(); //holds the indicies of which circles to eliminate
+        public static int diameter = 20;
+        public static int radius = 10;
+        public int delete_i; //holds index of which cirlce to delete
+
         private int circ_num = 0;
              
         public Form1()
@@ -32,36 +36,52 @@ namespace Lab3
                 this.circles.Add(c);
             }
 
-            if( e.Button == MouseButtons.Right )
+            if (e.Button == MouseButtons.Right)
             {
-                foreach (rbCircle C in this.circles)
+                //checks if the circle is black or red and takes the appropriate action
+                //black->red
+                //red->delete
+                int new_count = circ_num;
+                for (int i = 0; i < circ_num; i++)
                 {
-                    if (C.is_clicked(e.X, e.Y))
-                    { 
-                        if (C.color == 0)
-                        {
-                            C.color = 1;
-                        }
-                        else
-                        {
-                            circles.Remove(C);
-                        }
-                    }   
+                    if ((circles[i] as rbCircle).is_clicked(e.X, e.Y))
+                    {
+                        (circles[i] as rbCircle).mod_circle();
+                    }
+
                 }
             }
+            Invalidate();
+            
 
         }
 
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
+            //remove off circles and paint
             Graphics g = e.Graphics;
-
+            delete_i = 0;
             foreach( rbCircle C in circles )
             {
-                if( C.color == 0 )
+                if( C.color == 0 && C.on )
                 {
-                    g.FillEllipse(Brushes.Black, C.position.X - radius / 2, C.position.Y - radius / 2);
+                    g.FillEllipse(Brushes.Black, C.position.X , C.position.Y , diameter, diameter);
                 }
+                else if( C.on )
+                {
+                    g.FillEllipse(Brushes.Red, C.position.X , C.position.Y , diameter, diameter );
+                }
+                else
+                {
+                    remove_rbc.Add(delete_i);
+                }
+                delete_i++;
+       
+            }
+
+            foreach( int x in remove_rbc )
+            {
+                circles.Remove(x);
             }
 
         }
@@ -69,12 +89,14 @@ namespace Lab3
         public class rbCircle
         {
             public int color; // 0 is black and 1 is red
+            public bool on; // if true -- paint the circle, else don't
             public Point position; 
 
             public rbCircle( Point p )
             {
                 color = 0; //when a circle is first drawn, it is always black
-                position = new Point(p.X, p.Y);
+                position = new Point(p.X - radius, p.Y - radius);
+                on = true;
             }
 
             public void change_color()
@@ -84,13 +106,34 @@ namespace Lab3
 
             public bool is_clicked( int x, int y )
             {
-                int d = (position.X - x) ^ 2 + (position.Y - y) ^ 2;
-                if( d <= radius^2 )
+                double dx = (double)(position.X + radius - x);
+                double dy = (double)(position.Y + radius - y);
+                double d = Math.Pow(dx, 2) + Math.Pow(dy, 2);
+                d = Math.Sqrt(d);
+                if( radius > d )
                 {
                     return true;
                 }
                 return false;
             }
+
+            public void mod_circle( )
+            {
+                if (color == 0)
+                {
+                    color = 1;
+                }
+                else
+                {
+                    on = false;
+                }
+            }
+        }
+
+        private void Clear_Click(object sender, EventArgs e)
+        {
+            circles.Clear();
+            Invalidate();
         }
     }
 }
