2bd81b410d5ccdf60607cbc8172e9cc00c394eaf
andronovd <andronov@bu.edu>
2016-10-23 22:24:33 -0400

    work on lab5

---
+++ Lab5/WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Lab5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.options_panel = new System.Windows.Forms.Panel();
+            this.Outline = new System.Windows.Forms.CheckBox();
+            this.Fill = new System.Windows.Forms.CheckBox();
+            this.PenWidth = new System.Windows.Forms.Panel();
+            this.FillColor = new System.Windows.Forms.Panel();
+            this.PenColor = new System.Windows.Forms.Panel();
+            this.draw_box = new System.Windows.Forms.GroupBox();
+            this.draw_panel = new System.Windows.Forms.Panel();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.options_panel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // options_panel
+            // 
+            this.options_panel.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.options_panel.Controls.Add(this.Outline);
+            this.options_panel.Controls.Add(this.Fill);
+            this.options_panel.Controls.Add(this.PenWidth);
+            this.options_panel.Controls.Add(this.FillColor);
+            this.options_panel.Controls.Add(this.PenColor);
+            this.options_panel.Controls.Add(this.draw_box);
+            this.options_panel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.options_panel.Location = new System.Drawing.Point(0, 24);
+            this.options_panel.Name = "options_panel";
+            this.options_panel.Size = new System.Drawing.Size(601, 240);
+            this.options_panel.TabIndex = 0;
+            // 
+            // Outline
+            // 
+            this.Outline.AutoSize = true;
+            this.Outline.Location = new System.Drawing.Point(350, 190);
+            this.Outline.Name = "Outline";
+            this.Outline.Size = new System.Drawing.Size(59, 17);
+            this.Outline.TabIndex = 1;
+            this.Outline.Text = "Outline";
+            this.Outline.UseVisualStyleBackColor = true;
+            // 
+            // Fill
+            // 
+            this.Fill.AutoSize = true;
+            this.Fill.Location = new System.Drawing.Point(350, 160);
+            this.Fill.Name = "Fill";
+            this.Fill.Size = new System.Drawing.Size(38, 17);
+            this.Fill.TabIndex = 0;
+            this.Fill.Text = "Fill";
+            this.Fill.UseVisualStyleBackColor = true;
+            // 
+            // PenWidth
+            // 
+            this.PenWidth.Location = new System.Drawing.Point(480, 35);
+            this.PenWidth.Name = "PenWidth";
+            this.PenWidth.Size = new System.Drawing.Size(96, 165);
+            this.PenWidth.TabIndex = 0;
+            // 
+            // FillColor
+            // 
+            this.FillColor.Location = new System.Drawing.Point(375, 35);
+            this.FillColor.Name = "FillColor";
+            this.FillColor.Size = new System.Drawing.Size(80, 100);
+            this.FillColor.TabIndex = 0;
+            // 
+            // PenColor
+            // 
+            this.PenColor.Location = new System.Drawing.Point(270, 35);
+            this.PenColor.Name = "PenColor";
+            this.PenColor.Size = new System.Drawing.Size(80, 100);
+            this.PenColor.TabIndex = 0;
+            // 
+            // draw_box
+            // 
+            this.draw_box.Location = new System.Drawing.Point(35, 35);
+            this.draw_box.Name = "draw_box";
+            this.draw_box.Size = new System.Drawing.Size(210, 185);
+            this.draw_box.TabIndex = 0;
+            this.draw_box.TabStop = false;
+            this.draw_box.Text = "Draw";
+            // 
+            // draw_panel
+            // 
+            this.draw_panel.BackColor = System.Drawing.SystemColors.Control;
+            this.draw_panel.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.draw_panel.Location = new System.Drawing.Point(0, 264);
+            this.draw_panel.Name = "draw_panel";
+            this.draw_panel.Size = new System.Drawing.Size(601, 235);
+            this.draw_panel.TabIndex = 1;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.editToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(601, 24);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.clearToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // clearToolStripMenuItem
+            // 
+            this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Text = "Clear";
+            // 
+            // editToolStripMenuItem
+            // 
+            this.editToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.undoToolStripMenuItem});
+            this.editToolStripMenuItem.Name = "editToolStripMenuItem";
+            this.editToolStripMenuItem.Size = new System.Drawing.Size(39, 20);
+            this.editToolStripMenuItem.Text = "Edit";
+            // 
+            // undoToolStripMenuItem
+            // 
+            this.undoToolStripMenuItem.Name = "undoToolStripMenuItem";
+            this.undoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.undoToolStripMenuItem.Text = "Undo";
+            // 
+            // Lab5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(601, 499);
+            this.Controls.Add(this.draw_panel);
+            this.Controls.Add(this.options_panel);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Lab5";
+            this.Text = "andronov_Lab5";
+            this.options_panel.ResumeLayout(false);
+            this.options_panel.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel options_panel;
+        private System.Windows.Forms.Panel draw_panel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem clearToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem undoToolStripMenuItem;
+        private System.Windows.Forms.GroupBox draw_box;
+        private System.Windows.Forms.Panel PenColor;
+        private System.Windows.Forms.Panel FillColor;
+        private System.Windows.Forms.Panel PenWidth;
+        private System.Windows.Forms.CheckBox Fill;
+        private System.Windows.Forms.CheckBox Outline;
+    }
+}
+
---
+++ Lab5/WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Lab5 : Form
+    {
+        //ArrayList for container controls
+        ArrayList draw_rbs = new ArrayList();
+
+        //Draw_GroupBox controls
+        RadioButton line = new RadioButton();
+        RadioButton rect = new RadioButton();
+        RadioButton elip = new RadioButton();
+        RadioButton text = new RadioButton();
+        TextBox tb = new TextBox();
+
+        //create the 3 labels for each selection
+        Label pen_color = new Label();
+        Label fill_color = new Label();
+        Label pen_width = new Label();
+
+        //create the 3 listboxes for each label
+        ListBox pen_colors = new ListBox();
+        ListBox fill_colors = new ListBox();
+        ListBox pen_widths = new ListBox();
+
+        //data arrrays for the listboxes
+        private String[] pc_data = { "Black", "Red", "Blue", "Green" };
+        private String[] fl_data = { "White", "Black", "Red", "Blue", "Green" };
+        private int[] pw_data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+
+
+        public Lab5()
+        {
+            InitializeComponent();
+            //change Form size
+            Size s = new Size(600, 600);
+            this.Size = s;
+
+            //---The Draw GroupBox
+            //generate the controls in the Draw GroupBox
+            Point draw_startpoint = new Point(20, 20);
+            
+            //modify the radiobuttons & add them to the container
+            line.Text = "Line";
+            draw_rbs.Add(line);
+            rect.Text = "Rectangle";
+            draw_rbs.Add(rect);
+            elip.Text = "Ellipse";
+            draw_rbs.Add(elip);
+            text.Text = "Text";
+            draw_rbs.Add(text);
+
+            //modify the textbox
+            tb.Size = new Size(190, 65);
+            tb.Multiline = true;
+            tb.WordWrap = false;
+            tb.ScrollBars = ScrollBars.Both;
+            tb.AcceptsReturn = true;
+            tb.Dock = DockStyle.Bottom & DockStyle.Right;
+            draw_rbs.Add(tb);
+
+            //add the radiobuttons to the groupbox
+            foreach (Control c in draw_rbs)
+            {
+                draw_box.Controls.Add(c);
+                c.Location = draw_startpoint;
+                draw_startpoint.Y += 25;
+            }
+            
+            Point panel_pos = new Point(260, 35);
+            Point label_pt = new Point(0, 0);
+            //--The PenColor Panel
+            //add controls
+            PenColor.Controls.Add(pen_color);
+            PenColor.Controls.Add(pen_colors);
+            //adjust controls for PenColor panel
+            pen_color.Text = "Pen Color";
+            pen_color.Location = label_pt;
+            pen_colors.DataSource = pc_data;
+            pen_colors.Height = 75;
+            pen_colors.Dock = DockStyle.Bottom;
+
+            //--The FillColor Panel
+            //add controls
+            FillColor.Controls.Add(fill_color);
+            FillColor.Controls.Add(fill_colors);
+            //adjust controls for FillColor Panel
+            fill_color.Text = "Fill Color";
+            fill_color.Location = label_pt;
+            fill_colors.DataSource = fl_data;
+            fill_colors.Height = 75;
+            fill_colors.Dock = DockStyle.Bottom;
+
+            //--The PenWidth Panel
+            //add controls
+            PenWidth.Controls.Add(pen_width);
+            PenWidth.Controls.Add(pen_widths);
+            //adjust controls for PenWidth
+            pen_width.Text = "Pen Width";
+            pen_width.Location = label_pt;
+            pen_widths.DataSource = pw_data;
+            pen_widths.Height = 140;
+            pen_widths.Dock = DockStyle.Bottom;
+        }
+
+        //graphics object class inheirited from the Graphic Class
+        public class graphic
+        {
+            public Point loc1; //location of the 1st corner
+            public Point loc2; //location of the 2nd corner
+            public Pen pen; //its color
+
+            public graphic()
+            {
+                loc1 = new Point(0, 0);
+                loc2 = new Point(0, 0);
+                pen = new Pen(Color.Black, 1);
+            }
+
+            public graphic( Point loc1, Point loc2, Pen p )
+            {
+                this.loc1 = loc1;
+                this.loc2 = loc2;
+                this.pen = p;
+            }
+
+            public virtual void Draw(Graphics g)
+            { }
+        }
+
+        public class Line : graphic
+        {
+            //this is the line object
+            public Line() { }
+            public Line( Point loc1, Point loc2, Pen p )
+            {
+                this.loc1 = loc1;
+                this.loc2 = loc2;
+                this.pen = p;
+            }
+
+            override public void Draw( Graphics g )
+            {
+                g.DrawLine(pen, loc1, loc2);
+            }
+        }
+
+        public class Rect : graphic
+        {
+            //this is the Rect object
+            public int width;
+            public int height;
+            public Point loc; //location of the upper left corner
+
+            public Rect() { }
+            public Rect(Point loc1, Point loc2, Pen p)
+            {
+                this.loc1 = loc1;
+                this.loc2 = loc2;
+                this.pen = p;
+            }
+
+            override public void Draw(Graphics g)
+            {
+                width = Math.Abs(loc1.X - loc2.X);
+                height = Math.Abs(loc1.Y - loc2.Y);
+                if( (loc1.X >= loc2.X) && (loc1.Y <= loc2.Y ) )
+                {
+                    loc = loc1;
+                }
+                else
+                {
+                    loc = loc2;
+                }
+
+                g.DrawRectangle( pen, new Rectangle( loc.X, loc.Y, width, height) );
+            }
+        }
+    }
+}
