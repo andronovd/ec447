5e01fad144050747dc17c3ee8509db6672f44fe0
andronovd <andronov@bu.edu>
2016-10-28 14:10:49 -0400

    almost done with lab5

--- Lab5/WindowsFormsApplication1/Form1.Designer.cs
+++ Lab5/WindowsFormsApplication1/Form1.Designer.cs
@@ -34,14 +34,26 @@
             this.PenWidth = new System.Windows.Forms.Panel();
             this.FillColor = new System.Windows.Forms.Panel();
             this.PenColor = new System.Windows.Forms.Panel();
+            this.pen_colors = new System.Windows.Forms.ListBox();
             this.draw_box = new System.Windows.Forms.GroupBox();
+            this.tb = new System.Windows.Forms.TextBox();
+            this.text = new System.Windows.Forms.RadioButton();
+            this.elip = new System.Windows.Forms.RadioButton();
+            this.rect = new System.Windows.Forms.RadioButton();
+            this.line = new System.Windows.Forms.RadioButton();
             this.draw_panel = new System.Windows.Forms.Panel();
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.clearToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.editToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.undoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fill_colors = new System.Windows.Forms.ListBox();
+            this.pen_widths = new System.Windows.Forms.ListBox();
             this.options_panel.SuspendLayout();
+            this.PenWidth.SuspendLayout();
+            this.FillColor.SuspendLayout();
+            this.PenColor.SuspendLayout();
+            this.draw_box.SuspendLayout();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -69,6 +81,7 @@
             this.Outline.TabIndex = 1;
             this.Outline.Text = "Outline";
             this.Outline.UseVisualStyleBackColor = true;
+            this.Outline.CheckStateChanged += new System.EventHandler(this.Outline_CheckStateChanged);
             // 
             // Fill
             // 
@@ -79,9 +92,11 @@
             this.Fill.TabIndex = 0;
             this.Fill.Text = "Fill";
             this.Fill.UseVisualStyleBackColor = true;
+            this.Fill.CheckedChanged += new System.EventHandler(this.Fill_CheckedChanged);
             // 
             // PenWidth
             // 
+            this.PenWidth.Controls.Add(this.pen_widths);
             this.PenWidth.Location = new System.Drawing.Point(480, 35);
             this.PenWidth.Name = "PenWidth";
             this.PenWidth.Size = new System.Drawing.Size(96, 165);
@@ -89,6 +104,7 @@
             // 
             // FillColor
             // 
+            this.FillColor.Controls.Add(this.fill_colors);
             this.FillColor.Location = new System.Drawing.Point(375, 35);
             this.FillColor.Name = "FillColor";
             this.FillColor.Size = new System.Drawing.Size(80, 100);
@@ -96,19 +112,90 @@
             // 
             // PenColor
             // 
+            this.PenColor.Controls.Add(this.pen_colors);
             this.PenColor.Location = new System.Drawing.Point(270, 35);
             this.PenColor.Name = "PenColor";
             this.PenColor.Size = new System.Drawing.Size(80, 100);
             this.PenColor.TabIndex = 0;
             // 
+            // pen_colors
+            // 
+            this.pen_colors.FormattingEnabled = true;
+            this.pen_colors.Location = new System.Drawing.Point(28, 42);
+            this.pen_colors.Name = "pen_colors";
+            this.pen_colors.Size = new System.Drawing.Size(49, 56);
+            this.pen_colors.TabIndex = 3;
+            this.pen_colors.SelectedIndexChanged += new System.EventHandler(this.pen_colors_SelectedIndexChanged);
+            // 
             // draw_box
             // 
+            this.draw_box.Controls.Add(this.tb);
+            this.draw_box.Controls.Add(this.text);
+            this.draw_box.Controls.Add(this.elip);
+            this.draw_box.Controls.Add(this.rect);
+            this.draw_box.Controls.Add(this.line);
             this.draw_box.Location = new System.Drawing.Point(35, 35);
             this.draw_box.Name = "draw_box";
             this.draw_box.Size = new System.Drawing.Size(210, 185);
             this.draw_box.TabIndex = 0;
             this.draw_box.TabStop = false;
             this.draw_box.Text = "Draw";
+            // 
+            // tb
+            // 
+            this.tb.Location = new System.Drawing.Point(20, 111);
+            this.tb.Multiline = true;
+            this.tb.Name = "tb";
+            this.tb.Size = new System.Drawing.Size(190, 74);
+            this.tb.TabIndex = 7;
+            // 
+            // text
+            // 
+            this.text.AutoSize = true;
+            this.text.Location = new System.Drawing.Point(20, 88);
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(46, 17);
+            this.text.TabIndex = 6;
+            this.text.TabStop = true;
+            this.text.Text = "Text";
+            this.text.UseVisualStyleBackColor = true;
+            this.text.MouseClick += new System.Windows.Forms.MouseEventHandler(this.text_MouseClick);
+            // 
+            // elip
+            // 
+            this.elip.AutoSize = true;
+            this.elip.Location = new System.Drawing.Point(20, 65);
+            this.elip.Name = "elip";
+            this.elip.Size = new System.Drawing.Size(55, 17);
+            this.elip.TabIndex = 5;
+            this.elip.TabStop = true;
+            this.elip.Text = "Ellipse";
+            this.elip.UseVisualStyleBackColor = true;
+            this.elip.MouseClick += new System.Windows.Forms.MouseEventHandler(this.elip_MouseClick);
+            // 
+            // rect
+            // 
+            this.rect.AutoSize = true;
+            this.rect.Location = new System.Drawing.Point(20, 42);
+            this.rect.Name = "rect";
+            this.rect.Size = new System.Drawing.Size(74, 17);
+            this.rect.TabIndex = 4;
+            this.rect.TabStop = true;
+            this.rect.Text = "Rectangle";
+            this.rect.UseVisualStyleBackColor = true;
+            this.rect.MouseClick += new System.Windows.Forms.MouseEventHandler(this.rect_MouseClick);
+            // 
+            // line
+            // 
+            this.line.AutoSize = true;
+            this.line.Location = new System.Drawing.Point(20, 19);
+            this.line.Name = "line";
+            this.line.Size = new System.Drawing.Size(45, 17);
+            this.line.TabIndex = 3;
+            this.line.TabStop = true;
+            this.line.Text = "Line";
+            this.line.UseVisualStyleBackColor = true;
+            this.line.MouseClick += new System.Windows.Forms.MouseEventHandler(this.line_MouseClick);
             // 
             // draw_panel
             // 
@@ -118,6 +205,8 @@
             this.draw_panel.Name = "draw_panel";
             this.draw_panel.Size = new System.Drawing.Size(601, 235);
             this.draw_panel.TabIndex = 1;
+            this.draw_panel.Paint += new System.Windows.Forms.PaintEventHandler(this.draw_panel_Paint);
+            this.draw_panel.MouseClick += new System.Windows.Forms.MouseEventHandler(this.draw_panel_MouseClick);
             // 
             // menuStrip1
             // 
@@ -141,8 +230,9 @@
             // clearToolStripMenuItem
             // 
             this.clearToolStripMenuItem.Name = "clearToolStripMenuItem";
-            this.clearToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.clearToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
             this.clearToolStripMenuItem.Text = "Clear";
+            this.clearToolStripMenuItem.Click += new System.EventHandler(this.Clear);
             // 
             // editToolStripMenuItem
             // 
@@ -158,6 +248,24 @@
             this.undoToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
             this.undoToolStripMenuItem.Text = "Undo";
             // 
+            // fill_colors
+            // 
+            this.fill_colors.FormattingEnabled = true;
+            this.fill_colors.Location = new System.Drawing.Point(23, 44);
+            this.fill_colors.Name = "fill_colors";
+            this.fill_colors.Size = new System.Drawing.Size(57, 56);
+            this.fill_colors.TabIndex = 4;
+            this.fill_colors.SelectedIndexChanged += new System.EventHandler(this.fill_colors_SelectedIndexChanged);
+            // 
+            // pen_widths
+            // 
+            this.pen_widths.FormattingEnabled = true;
+            this.pen_widths.Location = new System.Drawing.Point(17, 109);
+            this.pen_widths.Name = "pen_widths";
+            this.pen_widths.Size = new System.Drawing.Size(76, 56);
+            this.pen_widths.TabIndex = 5;
+            this.pen_widths.SelectedIndexChanged += new System.EventHandler(this.pen_widths_SelectedIndexChanged);
+            // 
             // Lab5
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -171,6 +279,11 @@
             this.Text = "andronov_Lab5";
             this.options_panel.ResumeLayout(false);
             this.options_panel.PerformLayout();
+            this.PenWidth.ResumeLayout(false);
+            this.FillColor.ResumeLayout(false);
+            this.PenColor.ResumeLayout(false);
+            this.draw_box.ResumeLayout(false);
+            this.draw_box.PerformLayout();
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
@@ -193,6 +306,14 @@
         private System.Windows.Forms.Panel PenWidth;
         private System.Windows.Forms.CheckBox Fill;
         private System.Windows.Forms.CheckBox Outline;
+        private System.Windows.Forms.RadioButton elip;
+        private System.Windows.Forms.RadioButton rect;
+        private System.Windows.Forms.RadioButton line;
+        private System.Windows.Forms.RadioButton text;
+        private System.Windows.Forms.TextBox tb;
+        private System.Windows.Forms.ListBox pen_colors;
+        private System.Windows.Forms.ListBox pen_widths;
+        private System.Windows.Forms.ListBox fill_colors;
     }
 }
 
--- Lab5/WindowsFormsApplication1/Form1.cs
+++ Lab5/WindowsFormsApplication1/Form1.cs
@@ -15,28 +15,45 @@ namespace WindowsFormsApplication1
     {
         //ArrayList for container controls
         ArrayList draw_rbs = new ArrayList();
-
-        //Draw_GroupBox controls
-        RadioButton line = new RadioButton();
-        RadioButton rect = new RadioButton();
-        RadioButton elip = new RadioButton();
-        RadioButton text = new RadioButton();
-        TextBox tb = new TextBox();
-
+        
         //create the 3 labels for each selection
         Label pen_color = new Label();
         Label fill_color = new Label();
         Label pen_width = new Label();
 
         //create the 3 listboxes for each label
-        ListBox pen_colors = new ListBox();
-        ListBox fill_colors = new ListBox();
-        ListBox pen_widths = new ListBox();
 
         //data arrrays for the listboxes
         private String[] pc_data = { "Black", "Red", "Blue", "Green" };
         private String[] fl_data = { "White", "Black", "Red", "Blue", "Green" };
         private int[] pw_data = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+
+        //Variables
+        //mouse state variable
+        private bool mstate = true; //false -> first click, true ->second click
+        //on right mouse click, the state var is flipped
+
+        //graphics type state variable
+        private short gtype = 0; //default -> 0, no type selected
+        
+        //graphics object arraylist
+        ArrayList grry = new ArrayList();
+
+        //corner points
+        private Point c1 = new Point();
+        private Point c2 = new Point();
+
+        //pen used for drawing & brush for filling
+        Pen pen = new Pen(Color.Black, 1);
+        Brush brush = Brushes.White;
+
+        //text box text;
+        String tb_text;
+
+        //drawing options
+        short dopt = 0;
+
 
 
 
@@ -115,31 +132,52 @@ namespace WindowsFormsApplication1
             pen_widths.Dock = DockStyle.Bottom;
         }
 
+        //--CLASSES--//
         //graphics object class inheirited from the Graphic Class
         public class graphic
         {
             public Point loc1; //location of the 1st corner
             public Point loc2; //location of the 2nd corner
-            public Pen pen; //its color
+            public Pen gpen; //its color
+            public Brushes gbrush;
+            public short gdopt;
 
             public graphic()
             {
                 loc1 = new Point(0, 0);
                 loc2 = new Point(0, 0);
-                pen = new Pen(Color.Black, 1);
+                gpen = new Pen(Color.Black, 1);
             }
 
             public graphic( Point loc1, Point loc2, Pen p )
             {
                 this.loc1 = loc1;
                 this.loc2 = loc2;
-                this.pen = p;
+                this.gpen = p;
             }
 
             public virtual void Draw(Graphics g)
             { }
+
+            protected bool isUpperCorner(Point p1, Point p2)
+            {
+                //returns which point in the the uper left corner, as we see it
+                //false if p1
+                //true if p2
+                if ((p1.X <= p2.X) && (p1.Y <= p2.Y))
+                {
+                    Console.WriteLine(p1.ToString() + " is upper left of " + p2.ToString());
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine(p2.ToString() + " is upper left of " + p1.ToString());
+                    return false;
+                }
+            }
         }
 
+        //line graphcis object class
         public class Line : graphic
         {
             //this is the line object
@@ -148,15 +186,16 @@ namespace WindowsFormsApplication1
             {
                 this.loc1 = loc1;
                 this.loc2 = loc2;
-                this.pen = p;
+                this.gpen = (Pen)p.Clone();
             }
 
             override public void Draw( Graphics g )
             {
-                g.DrawLine(pen, loc1, loc2);
+                g.DrawLine(this.gpen, loc1, loc2);
             }
         }
 
+        //Rectangle graphics object class
         public class Rect : graphic
         {
             //this is the Rect object
@@ -165,18 +204,21 @@ namespace WindowsFormsApplication1
             public Point loc; //location of the upper left corner
 
             public Rect() { }
-            public Rect(Point loc1, Point loc2, Pen p)
+            public Rect(Point loc1, Point loc2, Pen p, Brushes b, short options)
             {
                 this.loc1 = loc1;
                 this.loc2 = loc2;
-                this.pen = p;
+                this.gpen = (Pen)p.Clone();
+                this.gdopt = options;
+                this.gbrush = b;
             }
 
             override public void Draw(Graphics g)
             {
                 width = Math.Abs(loc1.X - loc2.X);
                 height = Math.Abs(loc1.Y - loc2.Y);
-                if( (loc1.X >= loc2.X) && (loc1.Y <= loc2.Y ) )
+                Rectangle r = new Rectangle(loc.X, loc.Y, width, height);
+                if (isUpperCorner(loc1, loc2))
                 {
                     loc = loc1;
                 }
@@ -185,8 +227,287 @@ namespace WindowsFormsApplication1
                     loc = loc2;
                 }
 
-                g.DrawRectangle( pen, new Rectangle( loc.X, loc.Y, width, height) );
+                if (gdopt == 1)
+                {
+                    g.DrawRectangle(this.gpen, r);
+                }
+                else if (gdopt == 2)
+                {
+                    g.FillRectangle(this.gbrush, r);
+                }
+                else if (gdopt == 3)
+                {
+                    g.FillRectangle(this.gbrush, r);
+                    g.DrawRectangle(this.gpen, r);
+                }
+                //else neither fill nor outline have been selected and nothing will be drawn
+
             }
+        }
+        
+
+        public class Elip : graphic
+        {
+            //this is the Rect object
+            public int width;
+            public int height;
+            public Point loc; //location of the upper left corner
+
+            public Elip() { }
+            public Elip(Point loc1, Point loc2, Pen p, short options)
+            {
+                this.loc1 = loc1;
+                this.loc2 = loc2;
+                this.gpen = (Pen)p.Clone();
+                this.gdopt = options;
+            }
+
+            override public void Draw(Graphics g)
+            {
+                width = Math.Abs(loc1.X - loc2.X);
+                height = Math.Abs(loc1.Y - loc2.Y);
+                Rectangle r = new Rectangle(loc.X, loc.Y, width, height);
+                if ( isUpperCorner( loc1, loc2 ) )
+                {
+                    loc = loc1;
+                }
+                else
+                {
+                    loc = loc2;
+                }
+
+                if( gdopt == 1 )
+                {
+                    g.DrawEllipse(this.gpen, r);
+                }
+                else if( gdopt == 2 )
+                {
+                    g.FillEllipse(this.gbrush, r);
+                }
+                else if( gdopt == 3 )
+                {
+                    g.FillEllipse(this.gbrush, r);
+                    g.DrawEllipse(this.gpen, r);
+                }
+                //else neither fill nor outline have been selected and nothing will be drawn
+                
+            }
+        }
+
+        public class textString : graphic
+        {
+            //this is the line object
+            public textString() { }
+            public textString(Point loc1, Point loc2, Pen p)
+            {
+                this.loc1 = loc1;
+                this.loc2 = loc2;
+                this.gpen = (Pen)p.Clone();
+            }
+
+            override public void Draw(Graphics g)
+            {
+                //g.DrawString(this.gpen, loc1, loc2);
+            }
+        }
+
+ //---FUNCTIONS--//
+        //drawing panel mouse click event handler
+        private void draw_panel_MouseClick(object sender, MouseEventArgs e)
+        {
+            if( e.Button == MouseButtons.Left )
+            {
+                mstate = !mstate;
+                if (mstate)
+                {
+                    c2.X = e.X;
+                    c2.Y = e.Y;
+                    Console.WriteLine("The second corner is at " + c2.ToString());
+                    //this is the second click, need to create the objects and add it to the list
+                    createGrpc(); 
+                    draw_panel.Invalidate();
+                }
+                else
+                {
+                    //else this is the first click
+                    c1.X = e.X;
+                    c1.Y = e.Y;
+                    Console.WriteLine("The first corner is at " + c1.ToString());
+                } 
+            }
+        }
+
+        //Clear function
+        private void Clear()
+        {
+            //this clears the canvas, the graphics object queue, and resets the mouse state variable
+            Console.WriteLine("Clearing Canvas, reseting mouse and emptying arraylist");
+            mstate = true;
+            grry.Clear();
+            draw_panel.Invalidate();
+        }
+
+        //Draw event handler
+        public void draw_panel_Paint(object sender, PaintEventArgs e)
+        {
+            //draw the objects in the graphics object arraylist
+            Graphics g = e.Graphics;
+            foreach( graphic go in grry )
+            {
+                go.Draw( g );
+            }
+        }
+
+        //radiobutton click event handlers
+        private void line_MouseClick(object sender, MouseEventArgs e)
+        {
+            //type -> 1, draw lines
+            Console.WriteLine("You have selected the line");
+            gtype = 1;
+        }
+
+        private void rect_MouseClick(object sender, MouseEventArgs e)
+        {
+            //type -> 2, draw rectangles
+            Console.WriteLine("You have selected the rectangle");
+            gtype = 2;
+        }
+
+        private void elip_MouseClick(object sender, MouseEventArgs e)
+        {
+            //type -> 3, draw elipses
+            Console.WriteLine("You have selected the elipse");
+            gtype = 3;
+        }
+
+        private void text_MouseClick(object sender, MouseEventArgs e)
+        {
+            //type -> 4, draw text
+            Console.WriteLine("You have selected the text");
+            gtype = 4;
+        }
+
+        private void createGrpc()
+        {
+            //upon a second click, create a new graphics object and add it to the arraylist
+            if( gtype == 1 )
+            {
+                //create a line
+                Line l = new WindowsFormsApplication1.Lab5.Line(c1, c2, pen);
+                grry.Add(l);
+
+            }
+            else if (gtype == 2)
+            {
+                //create a rectangle
+                Rect r = new Rect(c1, c2, pen, brush, dopt);
+                grry.Add(r);
+            }
+            else if (gtype == 3)
+            {
+                //create an ellipse
+                Elip e = new Elip(c1, c2, pen, dopt);
+                 
+            }
+            else if (gtype == 4)
+            {
+                //create some text
+            }
+        }
+
+        private void pen_colors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set the pen's color to the one the use selected
+            //pen.Color
+            int i = pen_colors.SelectedIndex;
+            if (i == 0)
+            {
+                pen.Color = Color.Black;
+            }
+            else if (i == 1)
+            {
+                pen.Color = Color.Red;
+            }
+            else if (i == 2)
+            {
+                pen.Color = Color.Blue;
+            }
+            else if (i == 3)
+            {
+                pen.Color = Color.Green;
+            }
+
+            string s = pen_colors.SelectedValue.ToString();
+        }
+
+        private void fill_colors_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set the fill color
+            /*
+            int i = fill_colors.SelectedIndex;
+            if (i == 0)
+            {
+                pen.Color = Color.White;
+            }
+            else if (i == 1)
+            {
+                pen.Color = Color.Black;
+            }
+            else if (i == 2)
+            {
+                pen.Color = Color.Red;
+            }
+            else if (i == 3)
+            {
+                pen.Color = Color.Blue;
+            }
+            else if (i == 4)
+            {
+                pen.Color = Color.Green;
+            }
+            */
+            
+            string s = fill_colors.SelectedValue.ToString();
+        }
+
+        private void pen_widths_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //set the pen width
+            int i = pen_widths.SelectedIndex + 1;
+            pen.Width = i;
+        }
+
+        //menu item click event handler
+        private void Clear(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Fill_CheckedChanged(object sender, EventArgs e)
+        {
+            if( Fill.Checked )
+            {
+                //it is checked
+                dopt |= 1;
+            }
+            else
+            {
+                dopt &= 2;
+            }
+            Console.WriteLine("Dopt is now " + dopt.ToString());
+        }
+
+        private void Outline_CheckStateChanged(object sender, EventArgs e)
+        {
+            if( Outline.Checked )
+            {
+                dopt |= 2;
+            }
+            else
+            {
+                dopt &= 1;
+            }
+            Console.WriteLine("Dopt is now " + dopt.ToString());
         }
     }
 }
