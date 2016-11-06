c87003a9d93439091e238d6d89077be1bcb494fe
andronovd <andronov@bu.edu>
2016-11-05 20:59:33 -0400

    started work on Lab5

---
+++ Lab6/Lab6/gameEngine.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Drawing;
+using System.Collections;
+
+public class gameEngine
+{
+    //data fields
+    private ArrayList taken;
+    private ArrayList free;
+
+    //function members
+    public gameEngine()
+	{
+        Console.WriteLine("Starting Game Engine");
+        taken = new ArrayList();
+
+        //initialize the list of free spots
+        free = new ArrayList();
+        for( short i = 0; i < 9; i++ )
+        {
+            free.Add(i);
+        }
+    }
+
+    public void add( short spot )
+    {
+        //add the move to the board
+        Console.WriteLine("you clicked on spot number " + spot.ToString() );
+        
+        //is the move legal ?
+        if( isLegal(spot) )
+        {
+            //mark the spot as taken
+            this.taken.Add(spot);
+            this.free.Remove(spot);
+        }
+    }
+
+    public bool isLegal(short spot)
+    {
+        //detect is a spot that was just clicked was a legal move,
+        foreach (short i in free)
+        {
+            if (i == spot)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public bool isOver()
+    {
+        //checks if the game is over
+        Console.WriteLine("items in free: {0}", free.Count);
+        if( free.Count == 0 )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public short Move()
+    {
+        Random r = new Random();
+        short spot = (short)free[r.Next(free.Count)];
+        this.free.Remove(spot);
+        this.taken.Remove(spot);
+        Console.WriteLine("selected spot No.{0}", spot);
+        return spot;
+    }
+
+}
---
+++ Lab6/gameEngine.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+
+public class gameEngine
+{
+    //data fields
+    private int[] board = { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+
+    //function members
+    public gameEngine()
+	{
+       
+    }
+
+    public void whatIs()
+    {
+        Console.WriteLine("This is a game engine");
+    }
+
+    public void add( short spot )
+    {
+        Console.WriteLine("you clicked on spot number " + spot.ToString() );
+        //add the move to the board
+        this.board[spot] = 1;
+        foreach( int i in board )
+        {
+            Console.Write(i.ToString());
+        }
+        Console.WriteLine();
+    }
+
+    public bool isLegal( short spot )
+    {
+        //detect is a spot that was just clicked was a legal move
+        if( this.board[spot] )
+        {
+            Console.WriteLine("That was a not a legal move");
+            return false;
+        }
+        Console.WriteLine("That was a legal move");
+        return true;
+        
+    }
+}
