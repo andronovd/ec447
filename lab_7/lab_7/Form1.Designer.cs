71336623de5a0916632affc97da5abef2bbdb179
andronovd <andronov@bu.edu>
2016-11-28 10:42:17 -0500

    no error checking yet

---
+++ lab_7/lab_7/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace lab_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.filename_label = new System.Windows.Forms.Label();
+            this.key_label = new System.Windows.Forms.Label();
+            this.filename_tb = new System.Windows.Forms.TextBox();
+            this.key_tb = new System.Windows.Forms.TextBox();
+            this.Browse = new System.Windows.Forms.Button();
+            this.ec = new System.Windows.Forms.Button();
+            this.dc = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // filename_label
+            // 
+            this.filename_label.AutoSize = true;
+            this.filename_label.Location = new System.Drawing.Point(42, 28);
+            this.filename_label.Name = "filename_label";
+            this.filename_label.Size = new System.Drawing.Size(52, 13);
+            this.filename_label.TabIndex = 0;
+            this.filename_label.Text = "Filename:";
+            // 
+            // key_label
+            // 
+            this.key_label.AutoSize = true;
+            this.key_label.Location = new System.Drawing.Point(42, 87);
+            this.key_label.Name = "key_label";
+            this.key_label.Size = new System.Drawing.Size(28, 13);
+            this.key_label.TabIndex = 1;
+            this.key_label.Text = "Key:";
+            // 
+            // filename_tb
+            // 
+            this.filename_tb.Location = new System.Drawing.Point(45, 53);
+            this.filename_tb.Name = "filename_tb";
+            this.filename_tb.Size = new System.Drawing.Size(175, 20);
+            this.filename_tb.TabIndex = 2;
+            this.filename_tb.TextChanged += new System.EventHandler(this.filename_tb_TextChanged);
+            // 
+            // key_tb
+            // 
+            this.key_tb.Location = new System.Drawing.Point(45, 122);
+            this.key_tb.Name = "key_tb";
+            this.key_tb.Size = new System.Drawing.Size(175, 20);
+            this.key_tb.TabIndex = 3;
+            this.key_tb.TextChanged += new System.EventHandler(this.key_tb_TextChanged);
+            // 
+            // Browse
+            // 
+            this.Browse.Image = ((System.Drawing.Image)(resources.GetObject("Browse.Image")));
+            this.Browse.Location = new System.Drawing.Point(316, 45);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(38, 35);
+            this.Browse.TabIndex = 4;
+            this.Browse.UseVisualStyleBackColor = true;
+            this.Browse.Click += new System.EventHandler(this.Browse_Click);
+            // 
+            // ec
+            // 
+            this.ec.Location = new System.Drawing.Point(45, 168);
+            this.ec.Name = "ec";
+            this.ec.Size = new System.Drawing.Size(75, 23);
+            this.ec.TabIndex = 5;
+            this.ec.Text = "Encrypt";
+            this.ec.UseVisualStyleBackColor = true;
+            this.ec.Click += new System.EventHandler(this.ec_Click);
+            // 
+            // dc
+            // 
+            this.dc.Location = new System.Drawing.Point(145, 168);
+            this.dc.Name = "dc";
+            this.dc.Size = new System.Drawing.Size(75, 23);
+            this.dc.TabIndex = 6;
+            this.dc.Text = "Decrypt";
+            this.dc.UseVisualStyleBackColor = true;
+            this.dc.Click += new System.EventHandler(this.dc_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(475, 261);
+            this.Controls.Add(this.dc);
+            this.Controls.Add(this.ec);
+            this.Controls.Add(this.Browse);
+            this.Controls.Add(this.key_tb);
+            this.Controls.Add(this.filename_tb);
+            this.Controls.Add(this.key_label);
+            this.Controls.Add(this.filename_label);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label filename_label;
+        private System.Windows.Forms.Label key_label;
+        private System.Windows.Forms.TextBox filename_tb;
+        private System.Windows.Forms.TextBox key_tb;
+        private System.Windows.Forms.Button Browse;
+        private System.Windows.Forms.Button ec;
+        private System.Windows.Forms.Button dc;
+    }
+}
+
---
+++ lab_7/lab_7/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace lab_7
+{
+    public partial class Form1 : Form
+    {
+        public string filename;
+        public string key_str;
+        public byte[] key = new byte[8];
+        public short key_ptr = 0;
+        public int key_len = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //getting the text from the text boxes into local variables
+        private void filename_tb_TextChanged(object sender, EventArgs e)
+        {
+            filename = filename_tb.Text;
+            Console.WriteLine("filename: {0}", filename);
+        }
+
+        private void key_tb_TextChanged(object sender, EventArgs e)
+        {
+            key_str = key_tb.Text;
+            Console.WriteLine("key_str: {0}", key_str);
+            process_key();
+        }
+
+        private void Browse_Click(object sender, EventArgs e)
+        {
+            if( openFileDialog1.ShowDialog()  == DialogResult.OK )
+            {
+                filename_tb.Text = openFileDialog1.FileName;
+                filename = openFileDialog1.FileName;
+            }
+        }
+
+        private void process_key()
+        {
+            //process the key_string and put it in the byte array
+            int i = 0;
+            for(; i < 8; i++ )
+            {
+                key[i] = 0;
+            }
+
+            i = 0;
+            foreach( byte b in key_str )
+            {
+                key[i] += b;
+                i = (short)((i + 1) & 7);
+            }
+
+            /*
+            Console.Write("key: ");
+            foreach( byte b in key )
+            {
+                Console.Write("{0}, ", b);
+            }
+            Console.WriteLine();
+            */
+        }
+
+        private void decrypt()
+        {
+            //set up file streams an des encrypter
+            //try to open the file
+            FileStream encf = null;
+            try
+            {
+                encf = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to open file");
+            }
+
+            //open file for writing
+            //check if overwriting file
+            FileStream dest = new FileStream( filename + ".dec", FileMode.Create, FileAccess.Write);
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream dencF = new CryptoStream(encf, des.CreateDecryptor(key, key), CryptoStreamMode.Read);
+
+
+            int fLen = (int)encf.Length;
+            if (fLen == 0)
+            {
+                Console.WriteLine("File is empty");
+                return;
+            }
+            byte[] b = new byte[1];
+            int offset = 0;
+            do
+            {
+                dencF.Read(b, 0, 1);
+                dest.Write(b, 0, 1);
+                offset++;
+                fLen--;
+                Console.WriteLine("{0} bytes decrypted", offset);
+            }
+            while (fLen > 0);
+
+            //close the file streams
+            encf.Close();
+            dencF.Close();
+            dest.Close();
+        }
+
+        private void encrypt()
+        {
+            //set up file streams an des encrypter
+            //try to open the file
+            FileStream f = null;
+            try
+            {
+                f = new FileStream(filename, FileMode.Open, FileAccess.Read);
+            }
+            catch( Exception e )
+            {
+                MessageBox.Show("Unable to open file");
+            }
+            
+            //open file for writing
+            //check if overwriting file
+            FileStream dest = new FileStream(filename + ".des", FileMode.Create, FileAccess.Write);
+            DES des = new DESCryptoServiceProvider();
+            CryptoStream encF = new CryptoStream(dest, des.CreateEncryptor(key, key), CryptoStreamMode.Write);
+            
+
+            int fLen = (int)f.Length;
+            if( fLen == 0 )
+            {
+                Console.WriteLine("File is empty");
+                return;
+            }
+            byte[] b = new byte[1];
+            int offset = 0;
+            int n;
+            do
+            {
+                n = f.Read(b, 0, 1);
+                encF.Write(b, 0, 1);
+                offset++;
+                fLen--;
+                Console.WriteLine("{0} bytes encrypted", offset);
+            }
+            while (fLen > 0);
+
+            //close the file streams
+            encF.Close();
+            f.Close();
+            dest.Close();
+        }
+
+        private void ec_Click(object sender, EventArgs e)
+        {
+            encrypt();
+        }
+
+        private void dc_Click(object sender, EventArgs e)
+        {
+            decrypt();
+        }
+    }
+}
