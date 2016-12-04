0c5821fea2cd5df181b27794e15096484056d703
andronovd <andronov@bu.edu>
2016-12-04 18:34:56 -0500

    starting work on lab 8

---
+++ Lab8/Lab8/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Lab8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileList = new System.Windows.Forms.ListBox();
+            this.buttonBox = new System.Windows.Forms.GroupBox();
+            this.del = new System.Windows.Forms.Button();
+            this.add = new System.Windows.Forms.Button();
+            this.show = new System.Windows.Forms.Button();
+            this.intervalLabel = new System.Windows.Forms.Label();
+            this.interval = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCollectionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonBox.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // fileList
+            // 
+            this.fileList.BackColor = System.Drawing.SystemColors.Window;
+            this.fileList.FormattingEnabled = true;
+            this.fileList.Location = new System.Drawing.Point(61, 40);
+            this.fileList.Name = "fileList";
+            this.fileList.Size = new System.Drawing.Size(472, 69);
+            this.fileList.TabIndex = 0;
+            // 
+            // buttonBox
+            // 
+            this.buttonBox.Controls.Add(this.del);
+            this.buttonBox.Controls.Add(this.add);
+            this.buttonBox.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.buttonBox.Location = new System.Drawing.Point(50, 153);
+            this.buttonBox.Name = "buttonBox";
+            this.buttonBox.Size = new System.Drawing.Size(217, 84);
+            this.buttonBox.TabIndex = 1;
+            this.buttonBox.TabStop = false;
+            this.buttonBox.Text = "Files";
+            // 
+            // del
+            // 
+            this.del.Location = new System.Drawing.Point(125, 31);
+            this.del.Name = "del";
+            this.del.Size = new System.Drawing.Size(75, 23);
+            this.del.TabIndex = 3;
+            this.del.Text = "Delete";
+            this.del.UseVisualStyleBackColor = true;
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(11, 31);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 2;
+            this.add.Text = "Add";
+            this.add.UseVisualStyleBackColor = true;
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(407, 184);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 4;
+            this.show.Text = "Show";
+            this.show.UseVisualStyleBackColor = true;
+            // 
+            // intervalLabel
+            // 
+            this.intervalLabel.AutoSize = true;
+            this.intervalLabel.Location = new System.Drawing.Point(372, 243);
+            this.intervalLabel.Name = "intervalLabel";
+            this.intervalLabel.Size = new System.Drawing.Size(42, 13);
+            this.intervalLabel.TabIndex = 5;
+            this.intervalLabel.Text = "Interval";
+            // 
+            // interval
+            // 
+            this.interval.Location = new System.Drawing.Point(420, 240);
+            this.interval.Name = "interval";
+            this.interval.Size = new System.Drawing.Size(62, 20);
+            this.interval.TabIndex = 6;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(592, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openCollectionToolStripMenuItem,
+            this.saveCollectionToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openCollectionToolStripMenuItem
+            // 
+            this.openCollectionToolStripMenuItem.Name = "openCollectionToolStripMenuItem";
+            this.openCollectionToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.openCollectionToolStripMenuItem.Text = "Open Collection";
+            // 
+            // saveCollectionToolStripMenuItem
+            // 
+            this.saveCollectionToolStripMenuItem.Name = "saveCollectionToolStripMenuItem";
+            this.saveCollectionToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.saveCollectionToolStripMenuItem.Text = "Save Collection";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(160, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(592, 291);
+            this.Controls.Add(this.interval);
+            this.Controls.Add(this.intervalLabel);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.buttonBox);
+            this.Controls.Add(this.fileList);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.buttonBox.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox fileList;
+        private System.Windows.Forms.GroupBox buttonBox;
+        private System.Windows.Forms.Button del;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Label intervalLabel;
+        private System.Windows.Forms.TextBox interval;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCollectionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+    }
+}
+
